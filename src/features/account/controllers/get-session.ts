e63be63b5f771c1040f02a45ad1321aad47b96c2
europaadvanced <europa.advanced@gmail.com>
2025-09-30 19:02:38 +0200

    Update get-session.ts

--- src/features/account/controllers/get-session.ts
+++ src/features/account/controllers/get-session.ts
@@ -1,20 +1,8 @@
 'use server';
-import { cookies } from 'next/headers';
-import { createServerClient } from '@supabase/ssr';
+import { createSupabaseServerClient } from '@/libs/supabase/supabase-server-client';
 
 export async function getSession() {
-  const c = await cookies();
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get: (name: string) => c.get(name)?.value,
-        set: (name: string, value: string, options?: any) => c.set(name, value, options),
-        remove: (name: string, options?: any) => c.set(name, '', { ...options, maxAge: 0 }),
-      },
-    }
-  );
+  const supabase = await createSupabaseServerClient();
   const { data } = await supabase.auth.getSession();
   return data.session;
 }
