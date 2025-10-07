ce11392586a00aef4d1ccce2f48ef002d2446e35
europaadvanced <europa.advanced@gmail.com>
2025-10-07 19:55:05 +0200

    Create useLocalStorage.ts

---
+++ public/ga-tenders/components/hooks/useLocalStorage.ts
@@ -0,0 +1,29 @@
+
+
+// FIX: Import `React` to make its namespace available for type annotations.
+import React, { useState, useEffect } from 'react';
+
+function getStorageValue<T,>(key: string, defaultValue: T): T {
+  const saved = localStorage.getItem(key);
+  if (saved) {
+    try {
+      return JSON.parse(saved);
+    } catch (error) {
+      console.error("Error parsing localStorage key:", key, error);
+      return defaultValue;
+    }
+  }
+  return defaultValue;
+}
+
+export const useLocalStorage = <T,>(key:string, defaultValue: T): [T, React.Dispatch<React.SetStateAction<T>>] => {
+  const [value, setValue] = useState<T>(() => {
+    return getStorageValue(key, defaultValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
