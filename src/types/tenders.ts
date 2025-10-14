9acfbd1cd939c3bc77f6a362880bb80f2bac9677
europaadvanced <europa.advanced@gmail.com>
2025-10-14 19:41:43 +0200

    Create tenders.ts

---
+++ src/types/tenders.ts
@@ -0,0 +1,8 @@
+export type Tender = {
+  id: string;
+  title_ai: string | null;
+  description_long: string | null;
+  publication_date: string | null;
+  deadline_date: string | null;
+  link: string | null;
+};
